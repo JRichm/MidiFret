be07f94e8c2ce715610579bee9e8211becdfc929
JRichm <jblackenmusic@gmail.com>
2023-03-30 11:45:50 -0600

    updated endpoints

--- server/controller.js
+++ server/controller.js
@@ -13,6 +13,17 @@ const sequelize = new Sequelize(CONNECTION_STRING, {
     }
 });
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '8fcebd61630f476f99939534f9aac57b',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 const standardTuning = [40, 45, 50, 55, 59, 64];
 const noteTimeIncr = 48;
 
